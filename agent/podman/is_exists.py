e4f3053384479bbe0229ce41ce0db78166bc0beb
Grisha <grisha765@tuta.io>
2025-03-02 22:28:13 +0300

    [u] runtime error

--- agent/podman/is_exists.py
+++ agent/podman/is_exists.py
@@ -5,9 +5,12 @@ async def is_exists() -> bool:
     '''
     check if podman container exists
     '''
-    resp = await common.client.get(
-        url=f'{config.podman.api_url}/containers/{config.podman.container_name}/exists'
-    )
+    try:
+        resp = await common.client.get(
+            url=f'{config.podman.api_url}/containers/{config.podman.container_name}/exists'
+        )
+    except Exception:
+        raise RuntimeError('The podman service is not running.')
     if resp.status_code == 204:
         return True
     else:
